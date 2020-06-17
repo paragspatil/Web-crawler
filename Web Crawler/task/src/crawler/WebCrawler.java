40d8bab13500ac561279435689d637877ff13560
paragspatil <paragspatil123@gmail.com>
2020-06-17 17:39:49 +0530

    Web Crawler stage 3 commit

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,12 +1,15 @@
 package crawler;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
@@ -41,11 +44,36 @@ public class WebCrawler extends JFrame {
 
         //text area to show html
         JTextArea HtmlTextArea = new JTextArea();
-        HtmlTextArea.setBounds(20,80,700,550);
+        HtmlTextArea.setBounds(20,100,740,550);
         HtmlTextArea.setVisible(true);
         HtmlTextArea.setName("HtmlTextArea");
         HtmlTextArea.disable();
         add(HtmlTextArea);
+
+        //add title lable
+        JLabel TitleLabel = new JLabel();
+        TitleLabel.setName("TitleLabel");
+        TitleLabel.setBounds(100,60,500,60);
+        TitleLabel.setVisible(true);
+        //TitleLabel.setText("Title:");
+        Font font = new Font("Courier", Font.BOLD,12);
+        TitleLabel.setFont(font);
+        TitleLabel.setFont(TitleLabel.getFont().deriveFont(16f));
+        add(TitleLabel);
+
+
+        //a lable for word "Title"
+        JLabel Title = new JLabel();
+        Title.setBounds(20,60,80,60);
+        Title.setVisible(true);
+        Title.setText("Title: ");
+        Title.setFont(font);
+        Title.setFont(Title.getFont().deriveFont(16f));
+        add(Title);
+
+        //Regular expression for extracting title;
+        String Titleregex = "<title>.*<\\/title>";
+        Pattern pattern = Pattern.compile(Titleregex);
 
         // RunButton execution function
         RunButton.addActionListener( e -> {
@@ -53,6 +81,15 @@ public class WebCrawler extends JFrame {
             try (InputStream inputStream = new BufferedInputStream(new URL(url).openStream())) {
                 String siteText = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
                 HtmlTextArea.setText(siteText);
+                Matcher matcher = pattern.matcher(siteText);
+                if(matcher.find()){
+                    int start = matcher.start();
+                    int end = matcher.end();
+                    String Roughtitle = matcher.group();
+                    String title = siteText.substring(start+7,end-8);
+                    TitleLabel.setText(title);
+
+                }
             } catch (MalformedURLException malformedURLException) {
                 malformedURLException.printStackTrace();
             } catch (IOException ioException) {
